2e2671eafed65b4de170656145b34d787db725bc
pkorycin <pawel.korycinski@nokia.com>
2019-01-26 17:02:08 +0100

    Add Sklep6_TINT_4_2ns_Page10

---
+++ Lab4/Sklep_6/src/java/warstwa_biznesowa/entity/Produkt1.java
@@ -0,0 +1,132 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+// Persistence Entity Class
+
+package warstwa_biznesowa.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+
+/**
+ *
+ * @author Student
+ */
+@Entity
+public class Produkt1 implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nazwa;
+    private float cena;
+    private float promocja;
+    private String producent;
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date data_produkcji;
+
+    public Date getData_produkcji() {
+	return data_produkcji;
+    }
+
+    public void setData_produkcji(Date data_produkcji) {
+	this.data_produkcji = data_produkcji;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNazwa() {
+        return nazwa;
+    }
+
+    public void setNazwa(String nazwa) {
+        this.nazwa = nazwa;
+    }
+
+    public float getCena() {
+        return cena;
+    }
+
+    public void setCena(float cena) {
+        this.cena = cena;
+    }
+
+    public float getPromocja() {
+        return promocja;
+    }
+
+    public void setPromocja(float promocja) {
+        this.promocja = promocja;
+    }
+    
+    public String getProducent() {
+        return producent;
+    }
+
+    public void setProducent(String producent) {
+        this.producent = producent;
+    }
+
+    public float cena_brutto(){
+        float cena_brutto = cena * (1-promocja/100);
+        return cena_brutto;
+    }
+
+	@Override
+	public int hashCode() {
+		int hash = 7;
+		hash = 37 * hash + Objects.hashCode(this.id);
+		hash = 37 * hash + Objects.hashCode(this.nazwa);
+		hash = 37 * hash + Float.floatToIntBits(this.cena);
+		hash = 37 * hash + Float.floatToIntBits(this.promocja);
+		hash = 37 * hash + Objects.hashCode(this.producent);
+		return hash;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		final Produkt1 other = (Produkt1) obj;
+		if (Float.floatToIntBits(this.cena) != Float.floatToIntBits(other.cena)) {
+			return false;
+		}
+		if (this.promocja != other.promocja) {
+			return false;
+		}
+		if (!Objects.equals(this.nazwa, other.nazwa)) {
+			return false;
+		}
+		if (!Objects.equals(this.producent, other.producent)) {
+			return false;
+		}
+		return true;
+	}
+
+    @Override
+    public String toString() {
+        return "warstwa_biznesowa.entity.Produkt1[ id=" + id + " ]";
+    }
+}
---
+++ Lab4/Sklep_6/src/java/warstwa_internetowa/Managed_produkt.java
@@ -0,0 +1,147 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+// JavaServerFaces/JSF Managed Bean
+
+package warstwa_internetowa;
+
+import java.util.Date;
+import javax.ejb.EJB;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.convert.NumberConverter;
+import javax.faces.model.DataModel;
+import javax.faces.model.ListDataModel;
+import warstwa_biznesowa.Fasada_warstwy_biznesowej;
+
+/**
+ *
+ * @author Student
+ */
+@Named(value = "managed_produkt")
+@RequestScoped
+public class Managed_produkt {
+   
+    @EJB
+    private Fasada_warstwy_biznesowej fasada;
+    private String nazwa;
+    private float cena;
+    private float promocja;
+    private String producent;
+    private float cena_brutto;
+    private DataModel items;
+    private int stan = 1;
+    private Date data_produkcji;
+    private NumberConverter number_convert=new NumberConverter();
+    
+    public Managed_produkt() {
+    }
+    
+    public Fasada_warstwy_biznesowej getFasada(){
+        return fasada;
+    }
+
+    public void setFasada(Fasada_warstwy_biznesowej fasada){
+        this.fasada = fasada;
+    }
+    
+    public NumberConverter getNumber_convert(){
+	this.number_convert.setPattern("######.##zł");
+	return number_convert;
+    }
+
+    public String getNazwa() {
+        return nazwa;
+    }
+
+    public void setNazwa(String nazwa) {
+        this.nazwa = nazwa;
+    }
+
+    public float getCena() {
+        return cena;
+    }
+
+    public void setCena(float cena) {
+        this.cena = cena;
+    }
+    
+    public float getPromocja() {
+        return promocja;
+    }
+
+    public void setPromocja(float promocja) {
+        this.promocja = promocja;
+    }
+    
+    public float getCena_brutto() {
+        return cena_brutto;
+    }
+
+    public void setCena_brutto(float cena_brutto) {
+        this.cena_brutto = cena_brutto;
+    }
+    
+    public Date getData_produkcji() {
+ 	return data_produkcji;
+    }
+
+    public void setData_produkcji(Date data_produkcji) {
+	this.data_produkcji = data_produkcji;
+    }
+
+    public DataModel utworz_DataModel(){
+	return new ListDataModel(fasada.items());
+    }
+    
+    public DataModel getItems(){
+	if (items == null){
+	    items = utworz_DataModel();
+	}
+	return items;
+    }
+    
+    public void setItems(DataModel items){
+	this.items = items;
+    }
+    
+    public int getStan(){
+	return stan;
+    }
+    
+    public void setStan(int stan){
+	this.stan = stan;
+    }
+    
+    public String getProducent() {
+        return producent;
+    }
+
+    public void setProducent(String producent) {
+        this.producent = producent;
+    }
+
+    public String dodaj_produkt(){
+        String dane[] = {nazwa, ""+cena, ""+promocja, producent};
+        fasada.utworz_produkt(dane, data_produkcji);
+        dane_produktu();
+        return "rezultat2";
+    }
+
+    public void dane_produktu(){
+	stan = 1;
+        String[] dane = fasada.dane_produktu();
+	if (dane == null){
+	    stan = 0;
+	} else {
+            nazwa=dane[0];
+            cena=Float.parseFloat(dane[1]);
+            promocja=Float.parseFloat(dane[2]);
+	    producent=dane[3];
+            data_produkcji.setTime(Long.parseLong(dane[4]));
+	    cena_brutto=Float.parseFloat(dane[5]);
+	}
+    }
+}
